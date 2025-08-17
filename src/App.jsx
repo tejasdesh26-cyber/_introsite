c83cbd6790967be4b3d4c4be42fcf065f9bfaa38
tejasdesh26-cyber <tejasdesh26@gmail.com>
2025-08-18 02:43:03 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Welcome to My E-Commerce Site</h1>
+      <p>Deployed on Netlify with React + Vite</p>
+    </div>
+  )
+}
+
+export default App
